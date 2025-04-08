eba25f4f353827ba69455537ace5a43aefaf8991
Lars Hvam <larshp@hotmail.com>
2025-04-08 09:34:11 +0200

    wip

--- backend/express.ts
+++ backend/express.ts
@@ -1,12 +1,21 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
-const port = 8080
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const port = 8080;
+
+app.all("/{*splat}", (req, res) => {
+  res.send('Hello World!');
+  console.dir(req.method);
+
+  if (req.method === 'POST') {
+    console.log('POST request received');
+    console.dir(req.body);
+  }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`)
+});
