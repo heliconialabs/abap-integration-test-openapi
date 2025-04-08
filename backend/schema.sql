f2a85b2e5fd460451d7e96cd73b5e98c021c1bd3
Lars Hvam <larshp@hotmail.com>
2025-04-08 09:19:39 +0200

    update

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,14 +1,5 @@
-CREATE DATABASE sampledb;
-
-\c sampledb
-
-CREATE TABLE users (
+CREATE TABLE values (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(50),
-  email VARCHAR(100) UNIQUE
+  key VARCHAR(50),
+  name VARCHAR(100) UNIQUE
 );
-
-INSERT INTO users (name, email) VALUES
-  ('Alpha', 'alpha@example.com'),
-  ('Beta', 'beta@example.com'),
-  ('Gamma', 'gamma@example.com');
